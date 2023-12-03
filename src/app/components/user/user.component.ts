74073bc6f6ed77f7cb648fbc96cbcd8ffb2f17ee
Andrew Kurylyk <andronniy7@gmail.com>
2023-12-03 15:36:45 +0200

    It is works

---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,12 @@
+import {Component, Input} from '@angular/core';
+import {IUser} from "../../interface";
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent {
+  @Input()
+  user: IUser
+}
